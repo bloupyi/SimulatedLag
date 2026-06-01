263c72faafcd510db8c083caa57dcd2979775929
bloup <preteseille06@gmail.com>
2026-06-01 18:45:27 +0200

    fixed target always being the sender

--- src/main/java/fr/bloup/simulatedLag/commands/SimulatedLagCommand.java
+++ src/main/java/fr/bloup/simulatedLag/commands/SimulatedLagCommand.java
@@ -8,7 +8,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class SimulatedLagCommand extends AbstractCommand {
@@ -24,15 +23,25 @@ public class SimulatedLagCommand extends AbstractCommand {
 
     @Override
     public boolean runCommand(CommandSender sender, Command rootCommand, String label, String[] args) {
-        if (args.length > 0 && !Objects.equals(Bukkit.getPlayer(args[0]),null)) {
-            if (args.length > 1 && args[1].matches("-?\\d+")) {
-                plugin.setPlayerTargetedLag((Player) sender,Integer.parseInt(args[1]));
-                sender.sendMessage("[Lag] Targeted Ping of " + Bukkit.getPlayer(args[0]).getName() + " is now: " + Integer.parseInt(args[1]));
-            } else {
-                sender.sendMessage("[Lag] "+Bukkit.getPlayer(args[0]).getName()+"'s normal Ping: " + Bukkit.getPlayer(args[0]).getPing());
-                sender.sendMessage("[Lag] "+Bukkit.getPlayer(args[0]).getName()+"'s targeted Ping: " + plugin.targetedPlayerLag.get(Bukkit.getPlayer(args[0])));
-                sender.sendMessage("[Lag] "+Bukkit.getPlayer(args[0]).getName()+"'s packets delay: " + new DelayUtils(plugin).getDelayForPlayerLag(Bukkit.getPlayer(args[0])));
-            }
+        if (args.length == 0) {
+            sender.sendMessage("[Lag] Usage: /" + label + " <player> [ping]");
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            sender.sendMessage("[Lag] Player not found: " + args[0]);
+            return true;
+        }
+
+        if (args.length > 1 && args[1].matches("-?\\d+")) {
+            int ping = Integer.parseInt(args[1]);
+            plugin.setPlayerTargetedLag(target, ping);
+            sender.sendMessage("[Lag] Targeted Ping of " + target.getName() + " is now: " + ping);
+        } else {
+            sender.sendMessage("[Lag] " + target.getName() + "'s normal Ping: " + target.getPing());
+            sender.sendMessage("[Lag] " + target.getName() + "'s targeted Ping: " + plugin.targetedPlayerLag.get(target));
+            sender.sendMessage("[Lag] " + target.getName() + "'s packets delay: " + new DelayUtils(plugin).getDelayForPlayerLag(target));
         }
 
         return true;
